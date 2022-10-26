49cb5dbf359980fa48f1f77b6b56b22e708ab58f
Indra Cahya Ramdani <37335233+indracahyaramdani@users.noreply.github.com>
2022-10-26 15:23:43 +0700

    Add files via upload

---
+++ P1_Data Definition Language and Data Manipulation Langueage.sql
@@ -0,0 +1,22 @@
+-- Cara Membuat Tabel Sendiri
+
+CREATE TABLE indra (
+	NIM VARCHAR(10) PRIMARY KEY,
+	NamaDepan VARCHAR(10),
+	NamaBelakang VARCHAR(10),
+	NomorHP VARCHAR(14)
+)
+
+
+-- Cara Input Data
+
+INSERT INTO indra(NIM,NamaDepan,NamaBelakang,NomorHP)
+VALUES ('1800015053','Indra','Ramdani','082214720729')
+
+-- Cara melihat Data yang sudah di inputkan
+SELECT * FROM indra
+
+-- Tugas Insertkan nama teman kalian
+
+
+
